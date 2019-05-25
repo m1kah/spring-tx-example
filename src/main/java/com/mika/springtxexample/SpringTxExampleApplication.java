55970fb2eb0221bf2664b0c038e242ab0e0ee540
Mika Hamalainen <mika.hamalainen@gmail.com>
2019-05-25 07:06:19 +0300

    Project generated with initializr

---
+++ src/main/java/com/mika/springtxexample/SpringTxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mika.springtxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTxExampleApplication.class, args);
+    }
+
+}
